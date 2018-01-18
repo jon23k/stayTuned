d732527508fc2a334d793f3489df99aebd8f1a78
Jonathan Kurian <jonykurian@gmail.com>
2018-01-17 20:14:27 -0600

    added firebase key

--- stayTunedMedia/src/environments/environment.prod.ts
+++ stayTunedMedia/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconfig: {
+    apiKey: "AIzaSyBz4hFX5gd0tTcgmhiP0vOGF-hm8pxM6GU",
+    authDomain: "stay-tuned-media.firebaseapp.com",
+    databaseURL: "https://stay-tuned-media.firebaseio.com",
+    projectId: "stay-tuned-media",
+    storageBucket: "stay-tuned-media.appspot.com",
+    messagingSenderId: "732588943161"
+  }
 };
--- stayTunedMedia/src/environments/environment.ts
+++ stayTunedMedia/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseconfig: {
+    apiKey: "AIzaSyBz4hFX5gd0tTcgmhiP0vOGF-hm8pxM6GU",
+    authDomain: "stay-tuned-media.firebaseapp.com",
+    databaseURL: "https://stay-tuned-media.firebaseio.com",
+    projectId: "stay-tuned-media",
+    storageBucket: "stay-tuned-media.appspot.com",
+    messagingSenderId: "732588943161"
+  }
 };
