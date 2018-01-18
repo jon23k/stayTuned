17981e37d6c298100d892a7d2ac46a9dc41b241f
Jonathan Kurian <jonykurian@gmail.com>
2018-01-17 20:18:18 -0600

    added firebase modules

--- stayTunedMedia/src/app/app.module.ts
+++ stayTunedMedia/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
 import { AppRoutingModule } from './app-routing.module';
-
+import { environment } from "../environments/environment";
 import { AppComponent } from './app.component';
 
 
@@ -12,6 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseconfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
